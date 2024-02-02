39e9e2c289304b66d32e0f8ec4d2f2f6d69d481b
YS_Du <yansong.du@u.nus.edu>
2024-02-03 01:00:48 +0800

    data restructure

--- data_pre.py
+++ data_pre.py
@@ -1,2 +1,12 @@
 ###this is the pre-process data pipeline
 from data_ingestion import fetch_alpha_vantage_data, fetch_newsapi_data, fetch_github_repositories
+import json
+
+
+def pp_read_json(file_path):
+    with open(file_path, 'r') as file:
+        json.load(file)
+    return json.load(file)
+
+
+pp_read_json(file_path="")
